83cfbb42a5715cb3af60f3ff8006d2eb9ccef1ef
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-11-16 22:50:49 +0530

    second commit

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# React + Vite
+This App is to built to help gym goers for better exercise selection and Exercise gifs and videos. So, they can acheive better form while exercising.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Site Credit : Javascript Mastery
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import './App.css';
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <Box
+      width='400px'
+      sx={{ width: { xl: '1488px' } }}
+      m='auto'
+    >
+      <Navbar />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/exercise/:id'
+          element={<ExerciseDetail />}
+        />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
